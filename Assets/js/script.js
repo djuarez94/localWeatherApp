ba9ed9509f587db279def32aa3cbf690e65b93d3
David Juarez <DavidJuarez@Davids-MacBook-Pro.local>
2017-04-01 13:13:29 -0700

    changed openweather url

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -16,7 +16,7 @@ $( document ).ready(function() {
             $("#city").html(json.name);
             $("#main_weather").html(json.weather[0].main);
             $("#description_weather").html(json.weather[0].description);
-            $("#weather_image").attr("src", "https://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
+            $("#weather_image").attr("src", "http://openweathermap.org/img/w/" + json.weather[0].icon + ".png");
             $("#temperature").html(json.main.temp);
             $("#pressure").html(json.main.pressure);
             $("#humidity").html(json.main.humidity);
